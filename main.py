25e3abb210ed45cdb0aa58265b1f1733c775a859
unknown <rikcalderoni0000@gmail.com>
2025-05-14 19:26:43 +0200

    clear main

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from src.load_data import load_mnist_data
-
-
 def main():
-    train_loader, test_loader = load_mnist_data()
-    print(f"MNIST loaded with: {len(train_loader)} batch")
+    print("test")
 
 
 if __name__ == "__main__":
