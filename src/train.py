ea928aa1fc3cc53fab2caa3f0bdb7ca0fee14daa
unknown <rikcalderoni0000@gmail.com>
2025-05-15 19:10:29 +0200

    refactor code and add validation_loss

--- src/train.py
+++ src/train.py
@@ -3,32 +3,37 @@ from torch import optim
 from model import Mnist_Logistic
 from load_data import load_mnist_data
 from tqdm import tqdm
+import torch.nn.functional as F
 
+# Manually setting a seed will ensure that random values will be the same
 torch.manual_seed(42)
-model = Mnist_Logistic()
-optimizer = optim.SGD(model.parameters(), lr=0.01)
-loss_function = torch.nn.CrossEntropyLoss()
 
-train_loader, test_loader = load_mnist_data()
+# Setting our model as Mnist logistic - > softmax regression
+model = Mnist_Logistic()
+optimizer = optim.SGD(model.parameters(), lr=0.05)
+loss_function = F.cross_entropy
+
+
+train_loader, valid_loader, test_loader = load_mnist_data()
 print(f"MNIST loaded with: {len(train_loader)} batches")
 
 epochs = 5
 batch_size = train_loader.batch_size
-"""
-        - scorrere ogni batch
-          pred(train) - > forward()
-          loss = loss_func(pred,test_loader)
-          backprop 
-          with no grad
-            - aggiorno i pesi
-            -azzero i pesi
-    """
+
 for epoch in range(epochs):
+    # model.train()
     for xb, yb in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
         pred = model(xb.view(-1, 784))
         loss = loss_function(pred, yb)
-        loss.backward()
 
+        loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-    print(f"Epoch {epoch + 1}, Loss: {loss.item():.4f}")
+    # model.eval()
+    with torch.no_grad():
+        valid_loss = sum(
+            loss_function(model(xb.view(-1, 784)), yb) for xb, yb in valid_loader
+        ) / len(valid_loader)
+    print(
+        f"Epoch {epoch + 1}, Loss: {loss.item():.4f} , Validation Loss: {valid_loss:.4f}"
+    )
