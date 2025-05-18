51abfa9c2b23fc9d156e131012c5caf9cc15293d
unknown <rikcalderoni0000@gmail.com>
2025-05-18 19:15:14 +0200

    add simple CNN

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch.nn.functional as F
 
 
 class Mnist_Logistic(nn.Module):
@@ -8,3 +9,20 @@ class Mnist_Logistic(nn.Module):
 
     def forward(self, xb):
         return self.lin(xb)
+
+
+class Mnist_CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+
+    def forward(self, xb):
+        # convolution layers expect input with shape (batch size, channels, height, width)
+        xb = xb.view(-1, 1, 28, 28)
+        xb = F.relu(self.conv1(xb))
+        xb = F.relu(self.conv2(xb))
+        xb = F.relu(self.conv3(xb))
+        xb = F.avg_pool2d(xb, 4)
+        return xb.view(xb.size(0), -1)
