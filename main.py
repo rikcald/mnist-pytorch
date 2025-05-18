7d6146c48ee948c603fda330350cb7c5a01546b4
unknown <rikcalderoni0000@gmail.com>
2025-05-18 19:15:53 +0200

    choose mnistCNN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 from src.load_data import load_mnist_data
-from src.model import Mnist_Logistic
+from src.model import Mnist_Logistic, Mnist_CNN
 from src.train import fit
 import torch.nn.functional as F
 from torch import optim
@@ -11,10 +11,13 @@ def main():
     train_loader, valid_loader, test_loader = load_mnist_data()
     print(f"MNIST loaded with: {len(train_loader)} batches")
 
-    model = Mnist_Logistic()
-    optimizer = optim.SGD(model.parameters(), lr=0.05)
+    # model = Mnist_Logistic()
+    # optimizer = optim.SGD(model.parameters(), lr=0.05)
 
-    fit(5, model, F.cross_entropy, optimizer, train_loader, valid_loader)
+    model = Mnist_CNN()
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+
+    fit(10, model, F.cross_entropy, optimizer, train_loader, valid_loader)
 
 
 if __name__ == "__main__":
