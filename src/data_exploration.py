61803f0a0d6f3231921edba163cc0b699bf19437
unknown <rikcalderoni0000@gmail.com>
2025-05-20 17:37:51 +0200

    correct code for plotting random digit

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -1,21 +1,20 @@
 import torch
 import plotly.express as px
-import numpy as np 
+import numpy as np
 from load_data import load_mnist_data
 
-train_dl,_,_ = load_mnist_data()
-print(f"train_dl is a {type(train_dl)}")
+# Run this code to plot a random digit from the dataset
+train_dl, _, _ = load_mnist_data()
+
 
 batch = next(iter(train_dl))
 images, labels = batch
 
 
-print(f"Shape delle immagini: {images.shape}")   # es: torch.Size([64, 1, 28, 28])
-print(f"Shape delle etichette: {labels.shape}")  # es: torch.Size([64])
-print(f"Tipo delle immagini: {type(images)}")    # torch.Tensor
-print(f"Etichette: {labels[:10]}")   
+mnist_example = images[0][0].numpy()
 
-mnist_example = train_dl[42][0][0].numpy()
-print('A MNIST sample has size', mnist_example.shape)
+print("A MNIST sample has size", mnist_example.shape)
+
 fig = px.imshow(mnist_example)
+fig.update_layout(title=f"Etichetta: {labels[0].item()}")
 fig.show()
