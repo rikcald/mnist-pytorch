edec3122954da7926dab088a714161d671503d04
unknown <rikcalderoni0000@gmail.com>
2025-05-20 19:29:47 +0200

    add accuracy function

--- src/train.py
+++ src/train.py
@@ -12,7 +12,8 @@ def step(model, loss_func, xb, yb, opt=None):
         loss.backward()
         opt.step()
         opt.zero_grad()
-
+    else:
+        accuracy(pred, yb)
     return loss.item(), len(xb)
 
 
@@ -21,7 +22,7 @@ def fit(epochs, model, loss_function, optimizer, train_loader, valid_loader):
         model.train()
         for xb, yb in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
             xb_flat = xb.view(-1, 784)  # from [bs,1,28,28] to [bs,784]
-            loss, _ = step(model, loss_function, xb_flat, yb, optimizer)
+            loss, _ = step(model, loss_function, xb_flat, yb, opt=optimizer)
 
         model.eval()
         valid_loss = evaluate(model, loss_function, valid_loader)
@@ -44,3 +45,18 @@ def evaluate(model, loss_function, data_loader):
         )
         # Using NumPy for weighted average since losses and batch sizes are Python floats and ints, not PyTorch tensors
         return np.sum(np.multiply(losses, batch_sizes)) / np.sum(batch_sizes)
+
+
+def accuracy(pred, yb):
+    predicted_classes_perBatch = pred.argmax(dim=1)
+    correct_predictions_perBatch = (predicted_classes_perBatch == yb).sum().item()
+    # incorrect_predictions = len(yb) - correct_predictions_perBatch
+    accuracy_perBatch = correct_predictions_perBatch / len(yb)
+    print(
+        f"correct predictions : {correct_predictions_perBatch} / {len(yb)} | accuracy per batch : {accuracy_perBatch:.4f}"
+    )
+    # return correct_predictions_perBatch
+
+
+# TODO riportare la media di correct predictions e accuracy
+# TODO per ogni epoch mostrare qualche random image missclassificata
